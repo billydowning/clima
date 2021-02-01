f4656754078e6fc3fc9336f6af0832ad35921e8a
Billy Downing <wldowning@gmail.com>
2021-02-01 12:19:31 -0700

    added weather model file

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -27,7 +27,15 @@ struct WeatherManager {
             
             // 3. Give the session a task
             
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
             
             //4. Start the task
             
@@ -35,14 +43,23 @@ struct WeatherManager {
         }
     }
     
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
-        if error != nil {
-            print(error!)
-            return
+    func parseJSON(weatherData: Data){
+       let decoder = JSONDecoder()
+        do {
+           let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            
+            print(weather.conditionName)
+            print(weather.temperatureString)
+            
+        } catch {
+            print(error)
         }
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString!)
-        }
+        
     }
+    
 }
